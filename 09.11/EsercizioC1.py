cd0bec7be65d2a4b3172cb86d15039a3e96422b8
Martina Curcio <martinacurcio2000@gmail.com>
2024-09-11 11:08:00 +0200

    modifiche

--- 09.11/EsercizioC1.py
+++ 09.11/EsercizioC1.py
@@ -10,27 +10,23 @@ Un metodo distanza_da_origine che restituisca la distanza del punto dall'origine
 piano. '''
 
 class Punto:
-
-    from math import sqrt
-
+    
     def __init__(self, x, y):
         self.x=x
         self.y=y
 
-    def muovi(self):
-        dx=float(input("Inserisci spostamento dx: "))
-        dy=float(input("Inserisci spostamento dy: "))
+    def muovi(self, dx, dy):
         self.x=self.x+dx
         self.y=self.y+dy
         print("I nuovi punti sono:",(self.x, self.y))
     
     def distanza_da_origine(self):
-        distanza=sqrt(self.x**2 + self.y**2)
-        print("La distanza è",(self.x,self.y),"da (0,0) è", distanza)
+        distanza=self.x**2 + self.y**2
+        print("La distanza al quadrato è",(self.x,self.y),"da (0,0) è", distanza)
 
         
 Punto1=Punto(3,5)
 Punto2=Punto(-2,0.5)
 
-Punto1.muovi()
+Punto1.muovi(0,4)
 Punto2.distanza_da_origine()
