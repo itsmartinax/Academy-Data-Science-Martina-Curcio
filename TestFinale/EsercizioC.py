7074e334ae83e6bfd047ccfa07bc410d89cfafd5
Martina Curcio <martinacurcio2000@gmail.com>
2024-09-27 12:20:21 +0200

    Update EsercizioC.py

--- TestFinale/EsercizioC.py
+++ TestFinale/EsercizioC.py
@@ -1,8 +1,4 @@
 # ESERCIZIO CLASSI
-''' Crea una classe chiamata Libro. Questa classe dovrebbe avere:
-Tre attributi: titolo, autore e pagine.
-Un metodo descrizione che restituisca una stringa del tipo "Il libro 'titolo' è stato scritto
-da 'autore' e ha 'pagine' pagine." '''
 
 class Libro:
 
@@ -11,5 +7,4 @@ class Libro:
         self.autore=autore
         self.pagine=pagine
     
-    #def descrizione(self,):
-    #    print("Il libro",self.titolo,"è stato scritto da",self.autore,"e ha",self.pagine,"pagine.")
+
